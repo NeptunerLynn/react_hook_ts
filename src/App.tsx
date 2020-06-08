97c4396c5e884aed71152e9a2ef89b41ab2c3c1c
NeptunerLynn <leonora529@163.com>
2020-06-08 15:49:44 +0800

    路由管理

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import RoutersConfig from './routers';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <RoutersConfig />
+    );
+  }
+}
+
+export default App;
